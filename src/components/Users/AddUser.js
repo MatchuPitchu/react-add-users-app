61f70c29cfcf45a95b2823dc24897105a1dd45e8
MatchuPitchu <michael.flohr@posteo.de>
2021-09-28 14:05:13 +0200

    Use useRef

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
@@ -6,14 +6,30 @@ import styles from './AddUser.module.css';
 import ErrorModal from '../UI/ErrorModal';
 
 const AddUser = ({ onAddUser }) => {
-  const [username, setUsername] = useState('');
-  const [age, setAge] = useState(''); // read input of form is always string
+  // const [username, setUsername] = useState('');
+  // const [age, setAge] = useState(''); // read input of form is always string
   const [err, setErr] = useState();
+
+  // useRef: first time component is rendered, React sets value of nameRef and ageRef
+  // to a real DOM element (-> NOT the virtual DOM of React, so should not manipulate it,
+  // only React should) that is rendered based on the JSX element with ref attribute
+  // and connection to the wished variable;
+  // useful when you only want to read a value and never plan on changing anything,
+  // then you don't need useState
+  const nameRef = useRef();
+  const ageRef = useRef();
 
   const submitUserData = (e) => {
     e.preventDefault();
+
+    // 2) useRef way: use read values of ref DOM elements instead of states 'username' + 'age';
+    // BUT here not a good use case, because inputs are now uncontrolled
+    // (with useState and two way binding they would be controlled) components
+    const enteredName = nameRef.current.value;
+    const enteredAge = ageRef.current.value;
+
     // check if fields are empty
-    if (username.trim().length === 0 || age.trim().length === 0) {
+    if (enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
       setErr({
         title: 'Invalid input',
         message: 'Please enter a valid name and age (non-empty values).',
@@ -21,7 +37,7 @@ const AddUser = ({ onAddUser }) => {
       return;
     }
     // check if age (-> converted to num with "+") is smaller than 1
-    if (+age < 1) {
+    if (+enteredAge < 1) {
       setErr({
         title: 'Invalid age',
         message: 'Please enter a valid age (> 0).',
@@ -30,15 +46,20 @@ const AddUser = ({ onAddUser }) => {
     }
 
     // forward input data with function to parent component App.js
-    onAddUser(username, age);
+    onAddUser(enteredName, enteredAge);
+
+    // reset input fields when using refs
+    // normally DON'T manipulate real DOM, but here for resetting input fields it's admissible
+    nameRef.current.value = '';
+    ageRef.current.value = '';
 
     // reset input fields
-    setUsername('');
-    setAge('');
+    // setUsername('');
+    // setAge('');
   };
 
-  const usernameChangeHandler = ({ target }) => setUsername(target.value);
-  const ageChangeHandler = ({ target }) => setAge(target.value);
+  // const usernameChangeHandler = ({ target }) => setUsername(target.value);
+  // const ageChangeHandler = ({ target }) => setAge(target.value);
 
   const errHandler = () => setErr(null); // null is falsy value
 
@@ -60,15 +81,18 @@ const AddUser = ({ onAddUser }) => {
           <input
             id='username'
             type='text'
-            value={username}
-            onChange={usernameChangeHandler}
+            // value={username}
+            // onChange={usernameChangeHandler}
+            // connect HTML element to a ref const created above with useRef hook
+            ref={nameRef}
           />
           <label htmlFor='age'>Age (Years)</label>
           <input
             id='age'
             type='number'
-            value={age}
-            onChange={ageChangeHandler}
+            // value={age}
+            // onChange={ageChangeHandler}
+            ref={ageRef}
           />
           <Button type='submit'>Add User</Button>
         </form>
